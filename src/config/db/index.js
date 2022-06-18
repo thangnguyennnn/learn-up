8600393fcef97a408390f3ad87b832f889d13978
thangnguyennnn <thangnhde150387@fpt.edu.vn>
2022-06-18 22:42:01 +0700

    Update comment

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+// Tạo cấu hình kết nối MongoDB
 async function connect(){
     try {
         await mongoose.connect('mongodb://localhost:27017/Top_Learn_Nodejs');
